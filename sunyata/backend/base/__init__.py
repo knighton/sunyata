408054894ccb2ad00eec5b7c6b5253c8e8f52d14
James Knighton <iamknighton@gmail.com>
2017-11-12 11:55:56 -0800

    Pull out BaseMapAPI.

--- sunyata/backend/base/__init__.py
+++ sunyata/backend/base/__init__.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from .base import APIBase
 from .logic import BaseLogicAPI
+from .map import BaseMapAPI
 from .reduce import BaseReduceAPI
 
 
@@ -41,32 +42,6 @@ class BaseEpsilonAPI(APIBase):
 
     def epsilon(self):
         return self._epsilon
-
-
-class BaseMapAPI(APIBase):
-    def abs(self, x):
-        return self.maximum(x, -1 * x)
-
-    def neg(self, x):
-        return -1 * x
-
-    def sign(self, x):
-        return self.less(0, x) * 2 - 1
-
-    def clip(self, x, min=-np.inf, max=np.inf):
-        raise NotImplementedError
-
-    def log(self, x):
-        raise NotImplementedError
-
-    def pow(self, x, a):
-        raise NotImplementedError
-
-    def square(self, x):
-        return self.pow(x, 2)
-
-    def sqrt(self, x):
-        return self.pow(x, 0.5)
 
 
 class BaseMetricAPI(APIBase):
---
+++ sunyata/backend/base/map.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from .base import APIBase
+
+
+class BaseMapAPI(APIBase):
+    def abs(self, x):
+        return self.maximum(x, -1 * x)
+
+    def neg(self, x):
+        return -1 * x
+
+    def sign(self, x):
+        return self.less(0, x) * 2 - 1
+
+    def clip(self, x, min=-np.inf, max=np.inf):
+        raise NotImplementedError
+
+    def log(self, x):
+        raise NotImplementedError
+
+    def pow(self, x, a):
+        raise NotImplementedError
+
+    def square(self, x):
+        return self.pow(x, 2)
+
+    def sqrt(self, x):
+        return self.pow(x, 0.5)
