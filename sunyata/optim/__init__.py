6a8a248103f275188e9fb3a2395db62b0513b8ee
James Knighton <iamknighton@gmail.com>
2017-11-12 01:42:36 -0800

    Adagrad.

--- sunyata/optim/__init__.py
+++ sunyata/optim/__init__.py
@@ -1,3 +1,4 @@
+from .adagrad import *  # noqa
 from .nag import *  # noqa
 from .sgd import *  # noqa
 from .sgdm import *  # noqa
---
+++ sunyata/optim/adagrad.py
@@ -0,0 +1,25 @@
+from .. import backend as Z
+from .base import Optimizer
+
+
+class Adagrad(Optimizer):
+    def __init__(self, lr=0.01, decay_rate=0.99, epsilon=1e-6):
+        super().__init__()
+        assert 0 < lr
+        assert 0 < decay_rate < 1
+        assert 0 < epsilon
+        self.lr = lr
+        self.decay_rate = decay_rate
+        self.epsilon = epsilon
+
+    def make_context(self, variable):
+        return {
+            'lr': self.lr,
+            'decay_rate': self.decay_rate,
+            'epsilon': self.epsilon,
+            'cache': Z.zeros_like(variable),
+        }
+
+    def update_variable(self, var, grad, ctx):
+        ctx.cache = Z.square(grad)
+        Z.decr(var, ctx.lr * grad / Z.sqrt(ctx.cache + ctx.epsilon))
