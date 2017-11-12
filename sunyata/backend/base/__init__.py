9574b214afb5bf8291c28cd349c3d3a0970928a7
James Knighton <iamknighton@gmail.com>
2017-11-12 11:19:05 -0800

    Split out BaseReduceAPI.

--- sunyata/backend/base/__init__.py
+++ sunyata/backend/base/__init__.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from .base import APIBase
 from .logic import BaseLogicAPI
+from .reduce import BaseReduceAPI
 
 
 class Device(object):
@@ -83,51 +84,6 @@ class BaseMetricAPI(APIBase):
         hits = self.equal(true_indices, pred_indices)
         hits = self.cast(hits, self.dtype_of(true))
         return self.mean(hits, -1, False)
-
-
-class BaseReduceAPI(APIBase):
-    def argmin(self, axis=-1):
-        raise NotImplementedError
-
-    def argmax(self, axis=-1):
-        raise NotImplementedError
-
-    def min(self, x, axis=None, keepdims=False):
-        raise NotImplementedError
-
-    def max(self, x, axis=None, keepdims=False):
-        raise NotImplementedError
-
-    def sum(self, x, axis=None, keepdims=False):
-        raise NotImplementedError
-
-    def mean(self, x, axis=None, keepdims=False):
-        old_size = self.size(x)
-        x = self.sum(x, axis, keepdims)
-        ratio = old_size / self.size(x)
-        return x / ratio
-
-    def prod(self, x, axis=None, keepdims=False):
-        raise NotImplementedError
-
-    def var(self, x, axis=None, keepdims=False):
-        mean = self.mean(x, axis, keepdims)
-        var = self.square(x - mean)
-        return self.mean(var, axis, keepdims)
-
-    def std(self, x, axis=None, keepdims=False):
-        return self.sqrt(self.var(x, axis, keepdims))
-
-    def any(self, x, axis=None, keepdims=False, dtype=None):
-        nonneg = self.abs(x)
-        minima = self.min(nonneg, axis, keepdims)
-        return self.less(0, minima, dtype)
-
-    def all(self, x, axis=None, keepdims=False, dtype=None):
-        nonneg = self.abs(x)
-        nonzero = self.less(0, x, 'int64')
-        sums = self.sum(nonzero, axis, keepdims)
-        return self.less(0, x, dtype)
 
 
 class BaseRelateAPI(APIBase):
