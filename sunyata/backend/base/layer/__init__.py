e3d95f7c817081b7642b6aa49f6e1bcdbd950b75
James Knighton <iamknighton@gmail.com>
2017-11-18 04:36:38 -0800

    Fix.

--- sunyata/backend/base/layer/__init__.py
+++ sunyata/backend/base/layer/__init__.py
@@ -1,10 +1,11 @@
 from .activation import BaseActivationAPI
-from .batch_norm import BatchNormAPI
+from .batch_norm import BaseBatchNormAPI
 from .dot import BaseDotAPI
 from .shape import BaseShapeAPI
 
 
-class BaseLayerAPI(BaseActivationAPI, BaseDotAPI, BatchNormAPI, BaseShapeAPI):
+class BaseLayerAPI(BaseActivationAPI, BaseDotAPI, BaseBatchNormAPI,
+                   BaseShapeAPI):
     def __init__(self):
         BaseActivationAPI.__init__(self)
         BaseBatchNormAPI.__init__(self)
