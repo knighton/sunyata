7db462a3e921c3f79407e42d77df378fdcd5acec
James Knighton <iamknighton@gmail.com>
2017-11-22 00:36:40 -0800

    Refactor (batch norm backend API ndim usage).

--- sunyata/backend/base/layer/batch_norm.py
+++ sunyata/backend/base/layer/batch_norm.py
@@ -13,32 +13,39 @@ class BaseBatchNormAPI(APIMixin):
             x += beta
         return x
 
-    def instance_batch_norm(self, x, beta, gamma):
-        axes = [0] + list(range(2, self.ndim(x)))
-        mean, var = self.moments(x, axes)
+    def _instance_batch_norm(self, x, beta, gamma, ndim):
+        if ndim is None:
+            ndim = self.ndim(x) - 2
+        else:
+            assert self.ndim(x) - 2 == ndim
+        reduce_axes = [0] + list(range(2, ndim))
+        mean, var = self.moments(x, reduce_axes)
         return self.do_batch_norm(x, beta, gamma, mean, var)
 
+    def instance_batch_norm(self, x, beta, gamma):
+        return self._instance_batch_norm(x, beta, gamma, None)
+
     def instance_batch_norm0d(self, x, beta, gamma):
-        assert self.ndim(x) == 2
-        return self.instance_batch_norm(x, beta, gamma)
+        return self._instance_batch_norm(x, beta, gamma, 0)
 
     def instance_batch_norm1d(self, x, beta, gamma):
-        assert self.ndim(x) == 3
-        return self.instance_batch_norm(x, beta, gamma)
+        return self._instance_batch_norm(x, beta, gamma, 1)
 
     def instance_batch_norm2d(self, x, beta, gamma):
-        assert self.ndim(x) == 4
-        return self.instance_batch_norm(x, beta, gamma)
+        return self._instance_batch_norm(x, beta, gamma, 2)
 
     def instance_batch_norm3d(self, x, beta, gamma):
-        assert self.ndim(x) == 5
-        return self.instance_batch_norm(x, beta, gamma)
+        return self._instance_batch_norm(x, beta, gamma, 3)
 
-    def global_batch_norm(self, x, is_training, beta, gamma, momentum,
-                          moving_mean, moving_var, ndim=None):
+    def _global_batch_norm(self, x, is_training, beta, gamma, momentum,
+                           moving_mean, moving_var, ndim):
+        if ndim is None:
+            ndim = self.ndim(x) - 2
+        else:
+            assert self.ndim(x) - 2 == ndim
         if is_training:
-            axes = [0] + list(range(2, self.ndim(x)))
-            mean, var = self.moments(x, axes)
+            reduce_axes = [0] + list(range(2, ndim + 2))
+            mean, var = self.moments(x, reduce_axes)
             x = self.do_batch_norm(x, beta, gamma, mean, var)
             self.assign_momentum(moving_mean, mean, momentum)
             self.assign_momentum(moving_var, mean, momentum)
@@ -46,26 +53,27 @@ class BaseBatchNormAPI(APIMixin):
             x = self.do_batch_norm(x, beta, gamma, moving_mean, moving_var)
         return x
 
+    def global_batch_norm(self, x, is_training, beta, gamma, momentum,
+                          moving_mean, moving_var):
+        return self._global_batch_norm(x, is_training, beta, gamma, momentum,
+                                       moving_mean, moving_var, None)
+
     def global_batch_norm0d(self, x, is_training, beta, gamma, momentum,
                             moving_mean, moving_var):
-        assert self.ndim(x) == 2
-        return self.global_batch_norm(x, is_training, beta, gamma, momentum,
-                                      moving_mean, moving_var)
+        return self._global_batch_norm(x, is_training, beta, gamma, momentum,
+                                       moving_mean, moving_var, 0)
 
     def global_batch_norm1d(self, x, is_training, beta, gamma, momentum,
                             moving_mean, moving_var):
-        assert self.ndim(x) == 3
-        return self.global_batch_norm(x, is_training, beta, gamma, momentum,
-                                      moving_mean, moving_var)
+        return self._global_batch_norm(x, is_training, beta, gamma, momentum,
+                                       moving_mean, moving_var, 1)
 
     def global_batch_norm2d(self, x, is_training, beta, gamma, momentum,
                             moving_mean, moving_var):
-        assert self.ndim(x) == 4
-        return self.global_batch_norm(x, is_training, beta, gamma, momentum,
-                                      moving_mean, moving_var)
+        return self._global_batch_norm(x, is_training, beta, gamma, momentum,
+                                       moving_mean, moving_var, 2)
 
     def global_batch_norm3d(self, x, is_training, beta, gamma, momentum,
                             moving_mean, moving_var):
-        assert self.ndim(x) == 5
-        return self.global_batch_norm(x, is_training, beta, gamma, momentum,
-                                      moving_mean, moving_var)
+        return self._global_batch_norm(x, is_training, beta, gamma, momentum,
+                                       moving_mean, moving_var, 3)
