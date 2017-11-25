8adc39c71053c1ce60b921bf78188cf17cb81db2
James Knighton <iamknighton@gmail.com>
2017-11-24 17:13:10 -0800

    BinaryAccuracy metric.

--- sunyata/metric/accuracy.py
+++ sunyata/metric/accuracy.py
@@ -2,6 +2,11 @@ from .. import backend as Z
 from .base import Metric
 
 
+class BinaryAccuracy(Metric):
+    def __call__(self, true, pred):
+        return Z.binary_accuracy(true, pred)
+
+
 class CategoricalAccuracy(Metric):
     def __call__(self, true, pred):
         return Z.categorical_accuracy(true, pred)
