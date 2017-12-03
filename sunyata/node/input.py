d4b2aeb5280577cbf46c0ad790b356dd25551530
James Knighton <iamknighton@gmail.com>
2017-12-03 02:54:59 -0800

    Input.

---
+++ sunyata/node/input.py
@@ -0,0 +1,31 @@
+from .. import backend as Z
+from .base.pseudo_node import PseudoNode
+from .base.spec import Form
+
+
+class Input(PseudoNode):
+    """
+    Placeholder for model input data within a computational graph.
+
+    Input is one of the two kinds of PseudoNode (everything else is a Node).
+    """
+
+    def __init__(self, shape, dtype=None):
+        inputs = [self]
+        PseudoNode.__init__(self, inputs)
+        dtype = Z.dtype(dtype)
+        form = Form(shape, dtype)
+        self.initialize_forms([form])
+
+    def input_build(self):
+        for child in self.children():
+            child.node_build()
+
+    def input_params(self, nodes_seen, params_seen, params):
+        for child in self.children():
+            child.node_params(nodes_seen, params_seen, params)
+
+    def input_forward(self, x, is_training):
+        self.set_data([x])
+        for child in self.children():
+            child.node_forward(is_training)
